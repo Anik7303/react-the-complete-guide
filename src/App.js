0b31003b9062c0462f6e25e77553b88910685589
Anik7303 <anik7303@gmail.com>
2020-06-17 16:32:49 +0600

    defining basic setup for branches

--- src/App.js
+++ src/App.js
@@ -1,83 +1,14 @@
 import React, { Component } from "react";
-// import React, { useState } from "react";
 import "./App.css";
-import UserOutput from "./UserOutput/UserOutput";
-import UserInput from "./UserInput/UserInput";
 
 class App extends Component {
-    state = {
-        user: {
-            name: "Anik",
-        },
-    };
-
-    updateUsernameHandler(event) {
-        if (!event) {
-            return;
-        }
-        this.setState({
-            user: {
-                name: event.target.value,
-            },
-        });
-    }
-
     render() {
         return (
             <div className="App">
-                <UserInput
-                    updateUsername={this.updateUsernameHandler.bind(this)}
-                    username={this.state.user.name}
-                />
-                <UserOutput
-                    textOne="The first user-output component"
-                    username={this.state.user.name}
-                />
-                <UserOutput
-                    textOne="The second user-output component"
-                    username="Max"
-                />
+                <p>Welcome to REACT</p>
             </div>
         );
     }
 }
-
-// const App = (props) => {
-//     const [userState, setUserState] = useState({
-//         user: {
-//             name: "Anik",
-//             email: "anik@test.com",
-//         },
-//     });
-
-//     const updateUsernameHandler = (event) => {
-//         if (!event) {
-//             return;
-//         }
-//         setUserState({
-//             user: {
-//                 name: event.target.value,
-//                 email: "anik@test.com",
-//             },
-//         });
-//     };
-
-//     return (
-//         <div className="App">
-//             <UserInput
-//                 updateUsername={updateUsernameHandler.bind(this)}
-//                 username={userState.user.name}
-//             />
-//             <UserOutput
-//                 textOne="The first user-output component"
-//                 username={userState.user.name}
-//             />
-//             <UserOutput
-//                 textOne="The second user-output component"
-//                 username="Max"
-//             />
-//         </div>
-//     );
-// };
 
 export default App;
