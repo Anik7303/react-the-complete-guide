89f080000b47034f61ee7a51c6078cb1e498451a
Anik7303 <anik7303@gmail.com>
2020-06-22 01:11:34 +0600

    using prop-types module

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import CssClass from "./Person.css";
 
 class Person extends Component {
@@ -60,6 +61,13 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    click: PropTypes.func,
+    changeName: PropTypes.func,
+};
 
 export default Person;
 
